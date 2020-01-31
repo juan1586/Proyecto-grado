76821b171b5d73773f5040c99c0b582faaca1d3c
Juan pablo giraldo <jpg1586@gmail.com>
2020-01-30 19:51:39 -0500

    Inicio prueba

---
+++ MyFriendPet/ClientApp/src/app/components/pet-walker/pet-walker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-walker',
+  templateUrl: './pet-walker.component.html',
+  styleUrls: ['./pet-walker.component.css']
+})
+export class PetWalkerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ MyFriendPet/ClientApp/src/app/components/social-services/social-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-social-services',
+  templateUrl: './social-services.component.html',
+  styleUrls: ['./social-services.component.css']
+})
+export class SocialServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
